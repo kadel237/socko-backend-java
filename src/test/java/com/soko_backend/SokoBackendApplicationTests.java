3b668254053bc2c8933c67fd962d9a125f22bc3c
kadel237 <104805865+kadel237@users.noreply.github.com>
2025-07-15 16:47:01 +0200

    initialization of the project

---
+++ src/test/java/com/soko_backend/SokoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soko_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SokoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
